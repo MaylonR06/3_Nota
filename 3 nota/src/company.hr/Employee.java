082ca77b9ae4a877652cbddfcf0c82994b629709
MaylonR06 <maylonriano1@gmail.com>
2025-09-03 20:34:49 -0500

    Add files via upload

---
+++ 3 nota/src/company.hr/Employee.java
@@ -0,0 +1,31 @@
+package company.hr;
+
+public class Employee {
+    public String name;
+    protected String role;
+    private double salary;
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        if (salary > 0) {
+            this.salary = salary;
+        }
+    }
+
+    public void increaseSalary(double amount) {
+        if (amount > 0) {
+            this.salary += amount;
+        }
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
